218371a030ad9b1a4125690d119b6eb203d1c18e
xiaogaod325 <1135143763@qq.com>
2016-02-21 22:29:08 +0800

    changes log

--- download_mobi.py
+++ download_mobi.py
@@ -1,1 +1,16 @@
-__AUTHOR__ xiaogaod325
+__author__ = "xiaogaod325"
+import urllib2
+class  DOWNLOAD_MOBI:
+	def downLoad(self,attachmentUrl,attachmentName):
+		attachment=urllib2.urlopen(attachmentUrl)
+		#localfile=open(DIR+attachmentName,'wb')
+		#localfile.write(attachment.read())
+		#localfile.close()
+
+URL='http://www.hi-pda.com/forum/attachment.php?aid=MjQxNTIwN3wzNGU3MWZiNHwxNDU2MDY0MDUyfDNjZWZBczJjVGlnL2NLN1R3bklsTEYxM3JwZFJJUWQ2enRkYk1jMTM3em0yTGMw'
+NAME='reborn.mobi'
+DIR='/opt/nfs/scull'
+
+downloadmobi=DOWNLOAD_MOBI()
+downloadmobi.downLoad(URL,NAME)
+		
